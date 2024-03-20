c3902bb8cdb4d710ae3a066e491127a2ebbbbcee
Mikko2023 <124083316+Mikko2023@users.noreply.github.com>
2024-03-20 23:37:31 +0300

    Update task4.js

--- task/task4.js
+++ task/task4.js
@@ -6,43 +6,4 @@ let x = 552
 
 // Сумма чисел: 5 + 5 + 2 = 12 
 // 12 - четное число (true)
-Решение
-let a = 5
-switch (a) {
-  case 1:
-    alert( 'Зима' );
-    break;
-  case 2:
-    alert( 'Зима' );
-    break;
-  case 3:
-    alert( 'Весна' );
-    break;
-  case 4:
-    alert( 'Весна' );
-    break;
-  case 5:
-    alert( 'Весна' );
-    break;
-  case 6:
-    alert( 'Лето' );
-    break;
-  case 7:
-    alert( 'Лето' );
-    break;
-  case 8:
-    alert( 'Лето' );
-    break;
-  case 9:
-    alert( 'Осень' );
-    break;
-  case 10:
-    alert( 'Осень' );
-    break;
-  case 11:
-    alert( 'Осень' );
-    break;
-  case 12:
-    alert( 'Зима' );
-    break;
-}
+
