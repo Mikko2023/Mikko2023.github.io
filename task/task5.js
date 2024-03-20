0bf6b3f7412ec3e7e75864b0a63170873f662f01
Mikko2023 <124083316+Mikko2023@users.noreply.github.com>
2024-03-20 23:37:48 +0300

    Update task5.js

--- task/task5.js
+++ task/task5.js
@@ -1,3 +1,44 @@
 // todo: Дан номер месяца (1 — январь, 2 — февраль, ...). 
 // Вывести название соответствующего времени года("зима", "весна" и т.д.). 
 // Примечание: решить задачу через конструкцию switch
+
+Решение
+let a = 5
+switch (a) {
+  case 1:
+    alert( 'Зима' );
+    break;
+  case 2:
+    alert( 'Зима' );
+    break;
+  case 3:
+    alert( 'Весна' );
+    break;
+  case 4:
+    alert( 'Весна' );
+    break;
+  case 5:
+    alert( 'Весна' );
+    break;
+  case 6:
+    alert( 'Лето' );
+    break;
+  case 7:
+    alert( 'Лето' );
+    break;
+  case 8:
+    alert( 'Лето' );
+    break;
+  case 9:
+    alert( 'Осень' );
+    break;
+  case 10:
+    alert( 'Осень' );
+    break;
+  case 11:
+    alert( 'Осень' );
+    break;
+  case 12:
+    alert( 'Зима' );
+    break;
+}
