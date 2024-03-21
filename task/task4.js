407ec8172f1f5f2a22c498ff8de2ab19f6b2ee0c
Mikko2023 <124083316+Mikko2023@users.noreply.github.com>
2024-03-21 20:42:50 +0300

    Update task4.js

--- task/task4.js
+++ task/task4.js
@@ -7,3 +7,17 @@ let x = 552
 // Сумма чисел: 5 + 5 + 2 = 12 
 // 12 - четное число (true)
 
+------Начало решения -----
+  function sumOfDigit(num) { 
+    return num.toString().split("") 
+        .reduce((sum, digit) => 
+            sum + parseInt(digit), 0); 
+} 
+  
+console.log(sumOfDigit(525));
+
+const even = n => !(n % 2);
+
+console.info(even(5));
+console.info(even(4));
+
