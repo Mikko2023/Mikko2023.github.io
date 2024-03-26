bcc9a0a4775a7eae85c66cb4b0c574418b2f19b4
Mikko2023 <124083316+Mikko2023@users.noreply.github.com>
2024-03-26 19:35:03 +0300

    Update task10.js

--- task10.js
+++ task10.js
@@ -1,12 +1,1 @@
-//todo:  Задан произвольный url необходимо получить его домен.
-
-Пеример:
-var url = "http://www.ozon.ru/context/detail/id/19677670/"
-Домен: www.ozon.ru
-
------------------
-    Решение
-var link1 = "https://www.wildberries.ru/catalog/163855144/detail.aspx";
-var url = new URL(link1);
-var domain = url.hostname.split('.').slice(-3).join('.');
-console.log("The domain name is: " + domain);
+yt nj
