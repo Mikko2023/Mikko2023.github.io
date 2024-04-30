90bb133b0d4f797944542b0b9c48cc060288c97d
Mikko2023 <124083316+Mikko2023@users.noreply.github.com>
2024-04-30 12:39:08 +0300

    Add files via upload

---
+++ task/task7.js
@@ -0,0 +1,16 @@
+const queue = ['Иван', 'Мария', 'Саша', 'Даша'];
+const outside = ['Aмфибрахий'];
+
+// Удаление 'Иван' и 'Даша' из очереди
+queue.splice(queue.indexOf('Иван'), 1);
+queue.splice(queue.indexOf('Даша'), 1);
+
+// Добавление 'Амфибрахий' в очередь
+queue.push('Амфибрахий');
+
+
+outside.splice(outside.indexOf('Aмфибрахий'), 1);
+outside.push ('Иван', 'Даша');
+
+// Вывод результата в консоль
+console.log('В очереди: ' + queue + '\n' + 'Вне банка: ' + outside);
